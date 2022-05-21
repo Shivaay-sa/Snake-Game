1a5292a4acb83a3ccd1bf6d1cbb6d94487247a11
Shivaay-sa <shivam11112000agarwal@gmail.com>
2022-05-21 21:16:58 +0530

    adding high score

--- main.py
+++ main.py
@@ -34,14 +34,15 @@ while game_is_on:
 
     # Detecting collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.game_over()
-        game_is_on = False
+        scoreboard.reset()
+        snake.reset()
 
     # Detecting collision with tail
     for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
